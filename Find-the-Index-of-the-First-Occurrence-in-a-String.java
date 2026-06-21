94e829707442f05c9cbbcd7d6d5e4354a7552124
codingharsha <137827419+codingharsha@users.noreply.github.com>
2026-06-21 22:33:26 +0530

    [28] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- Find-the-Index-of-the-First-Occurrence-in-a-String.java
+++ Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -1,11 +1,10 @@
-class Solution {
-    public int strStr(String haystack, String needle) {
-        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
-            if(haystack.substring(i,j).equals(needle)){
-                return i;
-            }
-        }
-        return -1;
-    }
-}
-
+1class Solution {
+2    public int strStr(String haystack, String needle) {
+3        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+4            if(haystack.substring(i,j).equals(needle)){
+5                return i;
+6            }
+7        }
+8        return -1;
+9    }
+10}
